6d8573e3b79564eb54f000319c9a47077ad4143a
KUNAL CHOURE <chourekunal4@gmail.com>
2024-05-21 21:36:37 +0530

    updated

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 let body=document.querySelector("body");
 let h2=document.querySelector("h2");
 let started=false;
-let btn=["red","orange","green","blue"];
+let btns=["red","orange","green","blue"];
 let gameseq=[];
 let userseq=[];
 let level=0;
 body.addEventListener("keypress", function(){
      console.log("keypresed");
     if(started==false){
-        console.log("GAMESTARTED");
-        levelup();
+        console.log("GAMESTARTED");   
         started=true;
+        levelup();
 
      }
 })
 
 function levelup(){
+    body.style.backgroundColor="white";
+    userseq=[];
  level+=1;
  h2.innerText="LEVEL"+level;
 
  let randomindx=Math.floor(Math.random()*3);
- let randomcolor=btn[randomindx];
+ let randomcolor=btns[randomindx];
  let randbtn=document.querySelector(`.${randomcolor}`);
- console.log(randbtn);
+ /*console.log(randbtn);*/
  gameseq.push(randomcolor);
  console.log(gameseq);
  btnflash(randbtn);
@@ -30,31 +32,70 @@ function levelup(){
 }
 function btnflash(btn){
     btn.classList.add("flash");
-    
     setTimeout(function(){
         btn.classList.remove("flash");
-    },500 );
-    
+
+    },500)
+
 }
 
-let allbtn=document.querySelectorAll("btn");
-for(btn in allbtn){
-    btn.addEventListener("click",userclick());
+let allbtn=document.querySelectorAll(".btn");
+for(let btn of allbtn){
+    btn.addEventListener("click",userclick);
 }
 
 function userclick(){
+    console.log("clicked");
     let btn=this;
+   
     userflash(btn);
    let color=btn.getAttribute("id");
    userseq.push(color);
+ console.log(userseq);
 
-
+checkans(userseq.length-1);
 }
 
 function userflash(btn){
+   /* console.log(btn);*/
     btn.classList.add("userflash");
  setTimeout(function(){
-    btn.classList.remove("flash");
- },1000);
+    btn.classList.remove("userflash");
+ },500);
 
+}
+
+function checkans(indx){
+    console.log("LEVEL"+level);
+ 
+    if(userseq[indx]==gameseq[indx]){
+
+
+        if(userseq.length==gameseq.length){
+           
+            setTimeout(levelup,1000);
+        }
+    }
+
+    else{
+        h2.innerHTML=`GAME OVER YOUR SCORE IS:<b> ${level} </b><br>
+         PRESS ANY KEY TO RESTART`;
+         body.style.backgroundColor="red";
+         
+        setTimeout(function(){
+            restart();
+          
+        },500);
+
+
+    }
+
+}
+
+function restart(){
+    gameseq=[];
+    userseq=[];
+    level=0;
+    started=false;
+   
 }
